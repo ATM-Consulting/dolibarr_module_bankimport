ca4a7da58803b5ada60cfbf23d535efd2f7a9f04
atm-nicolasV <nicolas.vidal@atm-consulting.fr>
2026-01-30 09:11:26 +0100

    retours pr

--- tpl/bankimport.new.tpl.php
+++ tpl/bankimport.new.tpl.php
@@ -1,20 +1,20 @@
+/* Copyright (C) 2025 ATM Consulting
+*
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation; either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
 <fieldset>
 	<legend><?php print $langs->trans("ImportBankFile"); ?></legend>
-/* Copyright (C) 2025 ATM Consulting
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- */
 <form method="post" enctype="multipart/form-data" name="bankimport">
 	<input type="hidden" name="token" value=" <?php echo function_exists('newToken') ? newToken() : $_SESSION['newtoken'];?> ">
 	<table class="border" width="100%">
